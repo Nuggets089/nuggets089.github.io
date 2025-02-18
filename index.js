ec631dc91c2b04e6e6576c4343a9e61feceba1ba
Liz <lizdelora0808@gmail.com>
2025-02-18 15:03:50 -0500

    update html

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-alert("WELCOME TO THE AMAZING DIGITAL CIRCUS XD")
-alert(" ")
+let name =""
+name = prompt ("hi.what's your name?")
+alert("hi " + name)
+alert("nice name :D")
+let answer =""
+answer = prompt ("do you want to be my friend?")
+alert("thanks  " + name)
+alert("for being my friend now I will give you a gift")
+alert("Your gift will be 000000000.00000000.1% robux")
+alert("yoke you gift will be play with me whenever you want 😜")
+
